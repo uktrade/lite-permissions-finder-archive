908b065b5a7f07b5192f91c5a925c9b1d31e148e
Laurence Cartwright <laurence.cartwright@fivium.co.uk>
2016-09-27 10:48:31 +0100

    Fixed bug in control code summary regarding ancestor ordering

--- app/components/services/controlcode/frontend/FrontendServiceResult.java
+++ app/components/services/controlcode/frontend/FrontendServiceResult.java
@@ -1,32 +1,28 @@
 package components.services.controlcode.frontend;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
-import scala.Option;
+import com.google.common.collect.Lists;
 
+import java.util.Collections;
 import java.util.List;
-import java.util.stream.Collectors;
-import java.util.stream.IntStream;
+import java.util.Optional;
 
 public class FrontendServiceResult {
 
   public final ControlCodeData controlCodeData;
 
-  public final List<Ancestor> ancestors;
+  public final Optional<Ancestor> greatestAncestor;
+
+  public final List<Ancestor> otherAncestors;
 
   public FrontendServiceResult(@JsonProperty("controlCodeData") ControlCodeData controlCodeData,
                                @JsonProperty("lineage")List<Ancestor> ancestors) {
     this.controlCodeData = controlCodeData;
-    this.ancestors = ancestors;
-  }
-
-  public Option<Ancestor> getGreatestAncestor() {
-    return !ancestors.isEmpty() ? Option.apply(ancestors.get(ancestors.size() -1)) : Option.empty();
-  }
-
-  public List<Ancestor> getOtherAncestors() {
-    return IntStream.range(0, ancestors.size() - 1)
-        .mapToObj(i -> ancestors.get(i))
-        .collect(Collectors.toList());
+    List<Ancestor> reversedAncestors = Lists.reverse(ancestors);
+    this.greatestAncestor = !reversedAncestors.isEmpty() ? Optional.of(reversedAncestors.get(0)) : Optional.empty();
+    this.otherAncestors = reversedAncestors.size() > 1
+        ? reversedAncestors.subList(1, reversedAncestors.size())
+        : Collections.emptyList();
   }
 
 }
