5275b4c83b6bfada49f98576b7fe264850edebb5
Nick Palmer <nick.palmer@digital.bis.gov.uk>
2016-10-06 16:26:54 +0100

    Notification service client pass execution context

--- app/components/services/PermissionsFinderNotificationClient.java
+++ app/components/services/PermissionsFinderNotificationClient.java
@@ -3,20 +3,24 @@ package components.services;
 import com.google.common.collect.ImmutableMap;
 import com.google.inject.Inject;
 import components.common.client.NotificationServiceClient;
+import play.libs.concurrent.HttpExecutionContext;
 
 import java.util.Map;
 
 public class PermissionsFinderNotificationClient {
 
   private final NotificationServiceClient notificationServiceClient;
+  private final HttpExecutionContext httpExecutionContext;
 
   @Inject
-  public PermissionsFinderNotificationClient(NotificationServiceClient notificationServiceClient) {
+  public PermissionsFinderNotificationClient(NotificationServiceClient notificationServiceClient,
+                                             HttpExecutionContext httpExecutionContext) {
     this.notificationServiceClient = notificationServiceClient;
+    this.httpExecutionContext = httpExecutionContext;
   }
 
   public void sendApplicationReferenceEmail(String emailAddress, String applicationReference) {
     Map<String, String> params = ImmutableMap.of("applicationReference", applicationReference);
-    notificationServiceClient.sendEmail("permissionsFinder:applicationReference", emailAddress, params);
+    notificationServiceClient.sendEmail("permissionsFinder:applicationReference", emailAddress, params, httpExecutionContext);
   }
 }
