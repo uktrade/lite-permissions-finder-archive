12a9d0e80641751b8843f87d28149eb89e91d63e
Laurence Cartwright <laurence.cartwright@fivium.co.uk>
2016-10-07 15:31:19 +0100

    Added isHtml flag to EditSummaryField

--- app/components/services/ogels/registration/EditSummaryField.java
+++ app/components/services/ogels/registration/EditSummaryField.java
@@ -11,17 +11,21 @@ public class EditSummaryField {
   @JsonProperty("fieldValue")
   public final String fieldValue;
 
+  @JsonProperty("isHtml")
+  public final boolean isHtml;
+
   @JsonProperty("editLink")
   public final String editLink;
 
-  public EditSummaryField(String fieldName, String fieldValue, String editLink) {
+  public EditSummaryField(String fieldName, String fieldValue, boolean isHtml, String editLink) {
     this.fieldName = fieldName;
     this.fieldValue = fieldValue;
+    this.isHtml = isHtml;
     this.editLink = editLink;
   }
 
   public static EditSummaryField buildEditSummaryField (SummaryField summaryField) {
-    return new EditSummaryField(summaryField.heading, summaryField.content, summaryField.editLinkUrl);
+    return new EditSummaryField(summaryField.heading, summaryField.content, summaryField.isContentHtml, summaryField.editLinkUrl);
   }
 
 }
