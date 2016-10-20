d691376b15296881113a43ade0b222b6e5208f5f
Laurence Cartwright <laurence.cartwright@fivium.co.uk>
2016-10-20 17:17:11 +0100

    Destination country select hints are now labels associated with the ui-autocomplete country input

--- app/assets/javascripts/lite-permissions-finder.js
+++ app/assets/javascripts/lite-permissions-finder.js
@@ -1,5 +1,15 @@
 $(document).ready(function (){
-  $("[ui-autocomplete='ui-autocomplete']").selectToAutocomplete({"alternative-spellings-attr":"data-alternative-spelling", autoFocus:false});
+  $("select[ui-autocomplete='ui-autocomplete']").selectToAutocomplete({"alternative-spellings-attr":"data-alternative-spelling", "autoFocus":false});
+
+  // Associates the new ui-autocomplete input with the original select id (if the input was created), needed for labels and such.
+  $("select[ui-autocomplete='ui-autocomplete']").each(function() {
+    var id = $(this).attr("id");
+    var autocompleteInput = $("input[ui-autocomplete-id=" + id + "]");
+    if (autocompleteInput.length > 0) {
+      autocompleteInput.attr("id", id);
+      $(this).removeAttr("id");
+    }
+  });
 
   $("#through-destination-countries-wrapper").hide();
 
