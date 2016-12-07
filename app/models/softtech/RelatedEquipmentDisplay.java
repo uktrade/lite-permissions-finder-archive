bb94b9cb9b3212f64a85464e211d0d9236376455
Laurence Cartwright <laurence.cartwright@fivium.co.uk>
2016-12-07 11:20:20 +0000

    Added content to the related equipment view

--- app/models/softtech/RelatedEquipmentDisplay.java
+++ app/models/softtech/RelatedEquipmentDisplay.java
@@ -5,15 +5,18 @@ import controllers.softtech.routes;
 public class RelatedEquipmentDisplay {
   public final String formAction;
   public final String pageTitle;
+  public final String questionHeading;
 
   public RelatedEquipmentDisplay(GoodsType goodsType) {
     if (goodsType == GoodsType.SOFTWARE) {
       this.formAction = routes.RelatedEquipmentController.handleSubmit(goodsType.toUrlString()).url();
       this.pageTitle = "Software related to equipment or materials?";
+      this.questionHeading = "Is your software any of the following?";
     }
     else if (goodsType == GoodsType.TECHNOLOGY) {
       this.formAction = routes.RelatedEquipmentController.handleSubmit(goodsType.toUrlString()).url();
       this.pageTitle = "Technology related to equipment or materials?";
+      this.questionHeading = "Is your technology any of the following?";
     }
     else {
       throw new RuntimeException(String.format("Unexpected member of GoodsType enum: \"%s\""
