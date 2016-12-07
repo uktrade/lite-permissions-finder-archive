5f4d6bb61355579ab6500958f37cfa030fb3b8cb
Laurence Cartwright <laurence.cartwright@fivium.co.uk>
2016-12-07 11:31:32 +0000

    Added content to the software physical goods search view

--- app/models/search/SearchBaseDisplay.java
+++ app/models/search/SearchBaseDisplay.java
@@ -10,25 +10,37 @@ public class SearchBaseDisplay {
   public final Form<?> form;
   public final String formAction;
   public final String pageTitle;
+  public final String descriptionLabel;
+  public final String componentLabel;
+  public final String brandLabel;
+  public final String partNumberLabel;
 
   public SearchBaseDisplay(ControlCodeJourney controlCodeJourney, Form<?> form) {
     this.controlCodeJourney = controlCodeJourney;
     this.form = form;
     if (controlCodeJourney == ControlCodeJourney.PHYSICAL_GOODS_SEARCH) {
-      this.pageTitle = "Describe your items";
       this.formAction = routes.PhysicalGoodsSearchController.handleSearchSubmit().url();
+      this.pageTitle = "Describe your items";
+      this.descriptionLabel = "Provide as much information as you can";
+      this.componentLabel = "If the item is a component, attachment or part for another item, describe its use.";
     }
     else if (controlCodeJourney == ControlCodeJourney.PHYSICAL_GOODS_SEARCH_RELATED_TO_SOFTWARE) {
-      this.pageTitle = "Describe your items";
       this.formAction = routes.PhysicalGoodsSearchController.handleSearchRelatedToSubmit(GoodsType.SOFTWARE.toUrlString()).url();
+      this.pageTitle = "Describe the equipment or materials your software is related to";
+      this.descriptionLabel = "Provide as much information as you can about the physical item your software is used with";
+      this.componentLabel = "If the item your software is used with is a component, attachment or part for another item, describe the completed item or system.";
     }
     else if (controlCodeJourney == ControlCodeJourney.PHYSICAL_GOODS_SEARCH_RELATED_TO_TECHNOLOGY) {
-      this.pageTitle = "Describe your items";
       this.formAction = routes.PhysicalGoodsSearchController.handleSearchRelatedToSubmit(GoodsType.TECHNOLOGY.toUrlString()).url();
+      this.pageTitle = "Describe the equipment or materials your technology is related to";
+      this.descriptionLabel = "Provide as much information as you can about the physical item your technology is used with";
+      this.componentLabel = "If the item your technology is used with is a component, attachment or part for another item, describe the completed item or system.";
     }
     else {
       throw new RuntimeException(String.format("Unexpected member of ControlCodeJourney enum: \"%s\""
           , controlCodeJourney.toString()));
     }
+    this.brandLabel = "Brand or manufacturer";
+    this.partNumberLabel = "Part or model number";
   }
 }
