c9ac340b2e167fa46f4a777cde209c6fe55e2390
Laurence Cartwright <laurence.cartwright@fivium.co.uk>
2017-01-04 10:19:04 +0000

    Updated JourneyPrint test

--- test/journey/JourneyPrint.java
+++ test/journey/JourneyPrint.java
@@ -2,18 +2,21 @@ package journey;
 
 import components.common.journey.GraphvizSerialiser;
 import components.common.journey.JourneyDefinition;
+import journey.deciders.ControlCodeDecider;
+import journey.deciders.ExportCategoryDecider;
 import org.junit.Test;
 
 import java.util.Collection;
+import java.util.Collections;
 import java.util.Optional;
 
 public class JourneyPrint {
 
   @Test
   public void printExportJourney() {
-    Collection<JourneyDefinition> journeyDefinitions = new ExportJourneyDefinitionBuilder(new ControlCodeDecider(null, null)).buildAll();
+    Collection<JourneyDefinition> journeyDefinitions = new ExportJourneyDefinitionBuilder(new ControlCodeDecider(null, null), new ExportCategoryDecider(null)).buildAll();
     Optional<JourneyDefinition> serialiseJourney = journeyDefinitions.stream().filter(j -> JourneyDefinitionNames.EXPORT.equals(j.getJourneyName())).findFirst();
 
-    System.out.println(new GraphvizSerialiser().generateGraphvizSyntax(serialiseJourney.get()));
+    System.out.println(new GraphvizSerialiser().generateGraphvizSyntax(Collections.singletonList(serialiseJourney.get())));
   }
 }
