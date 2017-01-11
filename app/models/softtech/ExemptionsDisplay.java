2ea5879b1930348d9e67e075c582e9f29be2b995
Laurence Cartwright <laurence.cartwright@fivium.co.uk>
2017-01-11 11:49:35 +0000

    Content added to public domain software exemption question

--- app/models/softtech/ExemptionsDisplay.java
+++ app/models/softtech/ExemptionsDisplay.java
@@ -1,6 +1,7 @@
 package models.softtech;
 
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 import controllers.softtech.routes;
 
@@ -13,22 +14,25 @@ public class ExemptionsDisplay {
   }
 
   public final String formAction;
+  public final String pageTitle;
   public final String questionLabel;
   public final List<String> exemptions;
 
   public ExemptionsDisplay(ExemptionDisplayType exemptionDisplayType) {
     if (exemptionDisplayType == ExemptionDisplayType.Q1) {
-      this.questionLabel = "Do public domain software exemptions apply?";
+      this.pageTitle = "Public domain software";
+      this.questionLabel = "Is your software in the public domain, for example available as a free download that does not require permission or payment to use?";
       this.formAction = routes.ExemptionsController.handleSubmitQ1().url();
-      this.exemptions = Arrays.asList("Available to buy from retail outlets (e.g. shops open to the public)",
-          "In the public domain (free software that does not require permission or payment to use)");
+      this.exemptions = Collections.emptyList();
     }
     else if (exemptionDisplayType == ExemptionDisplayType.Q2) {
+      this.pageTitle = "Dummy";
       this.questionLabel = "Is software for information security?";
       this.formAction = routes.ExemptionsController.handleSubmitQ2().url();
       this.exemptions = Arrays.asList("Is your software for information security?");
     }
     else {
+      this.pageTitle = "Dummy";
       this.questionLabel = "Do software exemptions apply?";
       this.formAction = routes.ExemptionsController.handleSubmitQ3().url();
       this.exemptions = Arrays.asList("Software that the user can install themselves, without your help",
