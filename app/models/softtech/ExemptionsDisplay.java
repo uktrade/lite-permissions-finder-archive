85be5da5753872e314f398c897814e686e3ec0eb
Laurence Cartwright <laurence.cartwright@fivium.co.uk>
2017-01-11 13:36:29 +0000

    Content added to software information security question

--- app/models/softtech/ExemptionsDisplay.java
+++ app/models/softtech/ExemptionsDisplay.java
@@ -16,26 +16,26 @@ public class ExemptionsDisplay {
   public final String formAction;
   public final String pageTitle;
   public final String questionLabel;
-  public final List<String> exemptions;
+  public final List<String> questionContent;
 
   public ExemptionsDisplay(ExemptionDisplayType exemptionDisplayType) {
     if (exemptionDisplayType == ExemptionDisplayType.Q1) {
       this.pageTitle = "Public domain software";
       this.questionLabel = "Is your software in the public domain, for example available as a free download that does not require permission or payment to use?";
       this.formAction = routes.ExemptionsController.handleSubmitQ1().url();
-      this.exemptions = Collections.emptyList();
+      this.questionContent = Collections.emptyList();
     }
     else if (exemptionDisplayType == ExemptionDisplayType.Q2) {
-      this.pageTitle = "Dummy";
-      this.questionLabel = "Is software for information security?";
+      this.pageTitle = "Information security software";
+      this.questionLabel = "Are you exporting information security software?";
       this.formAction = routes.ExemptionsController.handleSubmitQ2().url();
-      this.exemptions = Arrays.asList("Is your software for information security?");
+      this.questionContent = Arrays.asList("This means software that ensures the accessibility, confidentiality or integrity of information or communications. It includes software for cryptography, cryptographic activation, cryptanalysis, protection against compromising emanations and computer security.");
     }
     else {
       this.pageTitle = "Dummy";
       this.questionLabel = "Do software exemptions apply?";
       this.formAction = routes.ExemptionsController.handleSubmitQ3().url();
-      this.exemptions = Arrays.asList("Software that the user can install themselves, without your help",
+      this.questionContent = Arrays.asList("Software that the user can install themselves, without your help",
           "Compiled source code that is the minimum needed to install, operate, maintain or repair exported items");
     }
 
