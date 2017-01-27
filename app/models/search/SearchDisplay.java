d544e97e52da214751291a22a042d03477159df3
Laurence Cartwright <laurence.cartwright@fivium.co.uk>
2017-01-27 11:47:56 +0000

    Minor typo fix

--- app/models/search/SearchDisplay.java
+++ app/models/search/SearchDisplay.java
@@ -7,7 +7,7 @@ public class SearchDisplay {
   public final String pageTitle;
   public final String descriptionLabel;
   public final String componentLabel;
-  public final String isComponenetLabel;
+  public final String isComponentLabel;
 
   public SearchDisplay(ControlCodeSubJourney controlCodeSubJourney) {
     this.controlCodeSubJourney = controlCodeSubJourney;
@@ -15,19 +15,19 @@ public class SearchDisplay {
       this.pageTitle = "Describe your item";
       this.descriptionLabel = "Provide as much information as you can";
       this.componentLabel = "Describe the equipment or system the item is designed to be part of";
-      this.isComponenetLabel = "Is your item a component, attachment or part for another item?";
+      this.isComponentLabel = "Is your item a component, attachment or part for another item?";
     }
     else if (controlCodeSubJourney == models.controlcode.ControlCodeSubJourney.PHYSICAL_GOODS_SEARCH_RELATED_TO_SOFTWARE) {
       this.pageTitle = "Describe the equipment or materials your software is related to";
       this.descriptionLabel = "Provide as much information as you can about the physical item your software is used with";
       this.componentLabel = "Describe the equipment or system the item is designed to be part of";
-      this.isComponenetLabel = "Is the item with which your software is used a component or part of a larger system?";
+      this.isComponentLabel = "Is the item with which your software is used a component or part of a larger system?";
     }
     else if (controlCodeSubJourney == models.controlcode.ControlCodeSubJourney.PHYSICAL_GOODS_SEARCH_RELATED_TO_TECHNOLOGY) {
       this.pageTitle = "Describe the equipment or materials your technology is related to";
       this.descriptionLabel = "Provide as much information as you can about the physical item your technology is used with";
       this.componentLabel = "Describe the equipment or system the item is designed to be part of";
-      this.isComponenetLabel = "Is the item with which your technology is used a component or part of a larger system?";
+      this.isComponentLabel = "Is the item with which your technology is used a component or part of a larger system?";
     }
     else {
       throw new RuntimeException(String.format("Unexpected member of ControlCodeSubJourney enum: \"%s\""
