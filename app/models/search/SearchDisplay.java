6b2a67ff2ac5174a89197e7cca1791fed9f5f2f2
Laurence Cartwright <laurence.cartwright@fivium.co.uk>
2017-01-27 09:56:26 +0000

    Content update to software/technology search view

--- app/models/search/SearchDisplay.java
+++ app/models/search/SearchDisplay.java
@@ -21,13 +21,13 @@ public class SearchDisplay {
       this.pageTitle = "Describe the equipment or materials your software is related to";
       this.descriptionLabel = "Provide as much information as you can about the physical item your software is used with";
       this.componentLabel = "Describe the equipment or system the item is designed to be part of";
-      this.isComponenetLabel = "Is the item your software is used with a component or part of a larger system?";
+      this.isComponenetLabel = "Is the item with which your software is used a component or part of a larger system?";
     }
     else if (controlCodeSubJourney == models.controlcode.ControlCodeSubJourney.PHYSICAL_GOODS_SEARCH_RELATED_TO_TECHNOLOGY) {
       this.pageTitle = "Describe the equipment or materials your technology is related to";
       this.descriptionLabel = "Provide as much information as you can about the physical item your technology is used with";
       this.componentLabel = "Describe the equipment or system the item is designed to be part of";
-      this.isComponenetLabel = "Is the item your technology is used with a component or part of a larger system?";
+      this.isComponenetLabel = "Is the item with which your technology is used a component or part of a larger system?";
     }
     else {
       throw new RuntimeException(String.format("Unexpected member of ControlCodeSubJourney enum: \"%s\""
