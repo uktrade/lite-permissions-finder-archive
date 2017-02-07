1f3d8b75b5c7fa4fb29ce4b579dbb2e7b17c5f59
Laurence Cartwright <laurence.cartwright@fivium.co.uk>
2017-02-07 15:26:17 +0000

    View specific javascript is now called as part of the view template, added a new standard page template "standardPageWithJavascript" to facilitate this

--- app/assets/javascripts/utils.js
+++ app/assets/javascripts/utils.js
@@ -1,46 +1,9 @@
 LITEPermissionsFinder.Utils = {
   ready: function() {
-    var pageName = LITEPermissionsFinder.Utils.getPageName();
-    if (pageName !== null && pageName != "undefined") {
-      if (pageName == "destinationCountry") {
-        LITEPermissionsFinder.DestinationCountry.setupPage();
-      }
-      if (pageName == "importCountry") {
-        LITEPermissionsFinder.ImportCountry.setupPage();
-      }
-      else if (pageName == "search") {
-        LITEPermissionsFinder.Search.setupPage();
-      }
-      else if (pageName == "searchResults") {
-        LITEPermissionsFinder.SearchResults.setupPage();
-      }
-      else if (pageName == "searchRelatedCodes") {
-        LITEPermissionsFinder.SearchRelatedCodes.setupPage();
-      }
-      else if (pageName == "selectExportCategories") {
-        LITEPermissionsFinder.ExportCategories.setupPage();
-      }
-      else if (pageName == "ogelQuestions") {
-        LITEPermissionsFinder.OgelQuestions.setupPage();
-      }
-      else if (pageName == "startApplication") {
-        LITEPermissionsFinder.StartApplication.setupPage();
-      }
-    }
+    // Triggered on $(document).ready()
   },
   load: function() {
-    var pageName = LITEPermissionsFinder.Utils.getPageName();
-    if (pageName !== null && pageName != "undefined") {
-      if (pageName == "searchResults") {
-        LITEPermissionsFinder.SearchResults.onload();
-      }
-      else if (pageName == "searchRelatedCodes") {
-        LITEPermissionsFinder.SearchRelatedCodes.onload();
-      }
-    }
-  },
-  getPageName: function() {
-    return $("meta[property='LITEPermissionsFinder:pageName']").attr("content");
+    // Triggered on $(window).load()
   },
   countrySetup: function() {
     $("select[ui-autocomplete='ui-autocomplete']").selectToAutocomplete({"alternative-spellings-attr":"data-alternative-spelling", "autoFocus":false});
