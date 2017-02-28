57614353b7ab306cecd3f4442fad732c144c0c6a
Laurence Cartwright <laurence.cartwright@fivium.co.uk>
2017-02-28 19:01:09 +0000

    ServiceClientLogger now logs the URL with parameters

--- app/components/services/ServiceClientLogger.java
+++ app/components/services/ServiceClientLogger.java
@@ -2,11 +2,21 @@ package components.services;
 
 import com.google.common.base.Stopwatch;
 import com.google.inject.Inject;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.utils.URIBuilder;
+import org.apache.http.message.BasicNameValuePair;
 import play.Logger;
 import play.libs.concurrent.HttpExecutionContext;
+import play.libs.ws.WSRequest;
 import play.libs.ws.WSRequestFilter;
 
+import java.net.MalformedURLException;
+import java.net.URISyntaxException;
+import java.util.Collection;
+import java.util.List;
+import java.util.Map;
 import java.util.concurrent.TimeUnit;
+import java.util.stream.Collectors;
 
 public class ServiceClientLogger {
   private final HttpExecutionContext httpExecutionContext;
@@ -16,9 +26,48 @@ public class ServiceClientLogger {
     this.httpExecutionContext = httpExecutionContext;
   }
 
+  /**
+   * Builds a URL from a {@link play.libs.ws.WSRequest} object
+   *
+   * @param request
+   * @return the URL with parameters, defaults to the requests {@link play.libs.ws.WSRequest#getUrl()} if the build is unsuccessful
+   */
+  private String requestToURL(WSRequest request) {
+    String url = request.getUrl();
+    Map<String, Collection<String>> queryParameters = request.getQueryParameters();
+    if (!queryParameters.isEmpty()) {
+      try {
+        URIBuilder uriBuilder = new URIBuilder(request.getUrl());
+        List<NameValuePair> nvpList = request.getQueryParameters().entrySet()
+            .stream()
+            .flatMap(entry -> entry.getValue()
+                .stream()
+                .map(value -> new BasicNameValuePair(entry.getKey(), value)))
+            .collect(Collectors.toList());
+        uriBuilder.addParameters(nvpList);
+        url = uriBuilder.build().toURL().toString();
+      }
+      catch (URISyntaxException e) {
+        Logger.error("Failed to build URI for request", e);
+      }
+      catch (MalformedURLException e) {
+        Logger.error("Failed to build URL for request", e);
+      }
+    }
+    return url;
+  }
+
+  /**
+   * Request filter for use with WSClient which logs outgoing http requests for use in service clients.
+   *
+   * @param serviceName name of the service which the request is sent too (used for logging purposes only)
+   * @param method HTTP method used in this request (used for logging purposes only)
+   * @return a request filter which logs the request
+   */
   public WSRequestFilter requestFilter(String serviceName, String method) {
     return executor -> request -> {
-      Logger.info(String.format("%s service request - URL: %s, method: %s", serviceName, request.getUrl(), method));
+      String url = requestToURL(request);
+      Logger.info(String.format("%s service request - URL: %s, method: %s", serviceName, url, method));
       Stopwatch stopwatch = Stopwatch.createStarted();
       return executor.apply(request)
           .thenApplyAsync(response -> {
