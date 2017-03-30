2560ed80cb33bd6babea7262625695489e8df707
Laurence Cartwright <laurence.cartwright@fivium.co.uk>
2017-03-30 12:09:33 +0100

    Added software dual use tests for the control code service /specific-controls endpoint

--- test/components/services/controlcode/category/CategoryControlsServiceConsumerPact.java
+++ test/components/services/controlcode/category/CategoryControlsServiceConsumerPact.java
@@ -43,7 +43,8 @@ public class CategoryControlsServiceConsumerPact {
     headers.put("Content-Type", "application/json");
 
     return builder
-        .uponReceiving("Existing software military controls")
+        .given("software exists as a goods type and there are military controls")
+        .uponReceiving("a request for software military controls")
           .path("/specific-controls/software/military")
           .method("GET")
           .willRespondWith()
@@ -53,13 +54,13 @@ public class CategoryControlsServiceConsumerPact {
         .toFragment();
   }
 
-  @Pact(provider = PactConfig.CONTROL_CODE_SERVICE_PROVIDER, consumer = PactConfig.CONSUMER, state = "empty")
+  @Pact(provider = PactConfig.CONTROL_CODE_SERVICE_PROVIDER, consumer = PactConfig.CONSUMER)
   public PactFragment softwareMilitaryEmpty(PactDslWithProvider builder) {
     Map<String, String> headers = new HashMap<>();
     headers.put("Content-Type", "application/json");
     return builder
-        .given("empty")
-        .uponReceiving("Empty software military controls")
+        .given("software exists as a goods type and no military controls exist")
+        .uponReceiving("a request for software military controls")
           .path("/specific-controls/software/military")
           .method("GET")
           .willRespondWith()
@@ -70,7 +71,7 @@ public class CategoryControlsServiceConsumerPact {
   }
 
   @Pact(provider = PactConfig.CONTROL_CODE_SERVICE_PROVIDER, consumer = PactConfig.CONSUMER)
-  public PactFragment technologyMilitary(PactDslWithProvider builder) {
+  public PactFragment softwareDualUse(PactDslWithProvider builder) {
     PactDslJsonArray existing = PactDslJsonArray.arrayEachLike(3)
         .stringType("controlCode")
         .stringType("title")
@@ -81,13 +82,30 @@ public class CategoryControlsServiceConsumerPact {
     headers.put("Content-Type", "application/json");
 
     return builder
-        .uponReceiving("Existing technology military controls")
-          .path("/specific-controls/technology/military")
+        .given("software exists as a goods type and telecoms exists as dual use category with controls")
+        .uponReceiving("a request for software dual use telecoms controls")
+          .path("/specific-controls/software/dual-use/telecoms")
           .method("GET")
           .willRespondWith()
             .status(200)
             .headers(headers)
             .body(existing)
+        .toFragment();
+  }
+
+  @Pact(provider = PactConfig.CONTROL_CODE_SERVICE_PROVIDER, consumer = PactConfig.CONSUMER)
+  public PactFragment softwareDualUseEmpty(PactDslWithProvider builder) {
+    Map<String, String> headers = new HashMap<>();
+    headers.put("Content-Type", "application/json");
+    return builder
+        .given("software exists as a goods type and telecoms exists as dual use category without controls")
+        .uponReceiving("a request for software dual use telecoms controls")
+          .path("/specific-controls/software/dual-use/telecoms")
+          .method("GET")
+          .willRespondWith()
+            .status(200)
+            .headers(headers)
+            .body(new PactDslJsonArray())
         .toFragment();
   }
 
@@ -126,18 +144,32 @@ public class CategoryControlsServiceConsumerPact {
   }
 
   @Test
-  @PactVerification(value = PactConfig.CONTROL_CODE_SERVICE_PROVIDER, fragment = "technologyMilitary")
-  public void testTechnologyMilitary() throws Exception {
+  @PactVerification(value = PactConfig.CONTROL_CODE_SERVICE_PROVIDER, fragment = "softwareDualUse")
+  public void testSoftwareDualUse() throws Exception {
     CategoryControlsServiceResult result;
     try {
-      result = client.get(GoodsType.TECHNOLOGY, SoftTechCategory.MILITARY).toCompletableFuture().get();
+      result = client.get(GoodsType.SOFTWARE, SoftTechCategory.TELECOMS).toCompletableFuture().get();
     }
     catch (InterruptedException | ExecutionException e) {
       throw new RuntimeException(e);
     }
     assertThat(result).isNotNull();
+    assertThat(result.getControlCodes().isEmpty()).isFalse();
   }
 
+  @Test
+  @PactVerification(value = PactConfig.CONTROL_CODE_SERVICE_PROVIDER, fragment = "softwareDualUseEmpty")
+  public void testSoftwareDualUseEmpty() throws Exception {
+    CategoryControlsServiceResult result;
+    try {
+      result = client.get(GoodsType.SOFTWARE, SoftTechCategory.TELECOMS).toCompletableFuture().get();
+    }
+    catch (InterruptedException | ExecutionException e) {
+      throw new RuntimeException(e);
+    }
+    assertThat(result).isNotNull();
+    assertThat(result.getControlCodes().isEmpty()).isTrue();
+  }
 
   @After
   public void tearDown() throws Exception {
