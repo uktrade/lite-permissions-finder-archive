274d5317abc09272a2b336077c4ed2be5c885b86
Laurence Cartwright <laurence.cartwright@fivium.co.uk>
2017-05-31 08:41:12 +0100

    Consumer pacts for lite-ogel-service /applicable-ogels endpoint

--- test/pact/PactConfig.java
+++ test/pact/PactConfig.java
@@ -4,4 +4,5 @@ public class PactConfig {
   public static final String CONSUMER = "lite-permissions-finder";
   public static final String CONTROL_CODE_SERVICE_PROVIDER = "lite-control-code-service";
   public static final String SEARCH_MANAGEMENT_PROVIDER = "lite-search-management";
+  public static final String OGEL_SERVICE_PROVIDER = "lite-ogel-service";
 }
