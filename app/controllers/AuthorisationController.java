1d8864fb5f534368fcda0b9e71eadd7e3cb664f3
Stefan Brach <brach.stefan@gmail.com>
2018-05-02 14:06:09 +0100

    PF-122 Added saml authorisation for test endpoint

---
+++ app/controllers/AuthorisationController.java
@@ -0,0 +1,35 @@
+package controllers;
+
+import static play.mvc.Controller.session;
+import static play.mvc.Results.forbidden;
+import static play.mvc.Results.ok;
+
+import com.google.inject.Inject;
+import components.common.CommonContextAction;
+import play.mvc.Result;
+import play.mvc.With;
+import views.html.auth.loggedOut;
+import views.html.auth.unauthorised;
+
+@With(CommonContextAction.class)
+public class AuthorisationController {
+
+  private final unauthorised unauthorised;
+  private final loggedOut loggedOut;
+
+  @Inject
+  public AuthorisationController(unauthorised unauthorised, loggedOut loggedOut) {
+    this.unauthorised = unauthorised;
+    this.loggedOut = loggedOut;
+  }
+
+  public Result unauthorised() {
+    return forbidden(unauthorised.render());
+  }
+
+  public Result loggedOut() {
+    session().clear();
+    return ok(loggedOut.render());
+  }
+
+}
