dbbfb483c030f9fc15c26ed5a1d0e6c21f778e71
Stefan Brach <brach.stefan@gmail.com>
2018-05-14 11:21:57 +0100

    PF-68 Added nested content

---
+++ app/models/view/NoteView.java
@@ -0,0 +1,15 @@
+package models.view;
+
+public class NoteView {
+
+  private final String text;
+
+  public NoteView(String text) {
+    this.text = text;
+  }
+
+  public String getText() {
+    return text;
+  }
+
+}
