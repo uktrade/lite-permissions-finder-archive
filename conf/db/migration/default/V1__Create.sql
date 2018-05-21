eecb02d51e6bcf1c447e3a6000754f4be08ee463
Laurence Cartwright <laurence.cartwright@fivium.co.uk>
2018-05-21 17:49:42 +0100

    Rolled V2 db migrate into V1

--- conf/db/migration/default/V1__Create.sql
+++ conf/db/migration/default/V1__Create.sql
@@ -1,7 +1,9 @@
 -- Types
 CREATE TYPE answer_type AS ENUM ('SELECT_ONE', 'SELECT_MANY');
 
-CREATE TYPE outcome_type AS ENUM ('TOO_COMPLEX', 'DECONTROL', 'CONTROL_ENTRY_FOUND');
+CREATE TYPE stage_outcome_type AS ENUM ('CONTROL_ENTRY_FOUND');
+
+CREATE TYPE stage_answer_outcome_type AS ENUM ('TOO_COMPLEX', 'DECONTROL', 'CONTROL_ENTRY_FOUND');
 
 CREATE TYPE note_type AS ENUM ('NB', 'NOTE', 'SEE_ALSO', 'TECH_NOTE');
 
@@ -47,28 +49,29 @@ CREATE TABLE note (
 );
 
 CREATE TABLE stage (
-  id                  BIGSERIAL       PRIMARY KEY,
-  journey_id          BIGINT          NOT NULL,
-  control_entry_id    BIGINT,
-  title               TEXT,
-  explanatory_notes   TEXT,
-  question_type       question_type   NOT NULL,
-  answer_type         answer_type     NOT NULL,
-  next_stage_id       BIGINT
+  id                        BIGSERIAL       PRIMARY KEY,
+  journey_id                BIGINT          NOT NULL,
+  control_entry_id          BIGINT,
+  title                     TEXT,
+  explanatory_notes         TEXT,
+  question_type             question_type   NOT NULL,
+  answer_type               answer_type     NOT NULL,
+  next_stage_id             BIGINT,
+  go_to_stage_outcome_type  stage_outcome_type
 );
 
 CREATE TABLE stage_answer (
-  id                  BIGSERIAL   PRIMARY KEY,
-  parent_stage_id     BIGINT      NOT NULL,
-  go_to_stage_id      BIGINT,
-  go_to_outcome_type  outcome_type,
-  control_entry_id    BIGINT,
-  answer_text         TEXT,
-  display_order       INT         NOT NULL,
-  answer_precedence   INT,
-  divider_above       BOOLEAN     NOT NULL,
-  nested_content      TEXT,
-  more_info_content   TEXT
+  id                                BIGSERIAL   PRIMARY KEY,
+  parent_stage_id                   BIGINT      NOT NULL,
+  go_to_stage_id                    BIGINT,
+  go_to_stage_answer_outcome_type   stage_answer_outcome_type,
+  control_entry_id                  BIGINT,
+  answer_text                       TEXT,
+  display_order                     INT         NOT NULL,
+  answer_precedence                 INT,
+  divider_above                     BOOLEAN     NOT NULL,
+  nested_content                    TEXT,
+  more_info_content                 TEXT
 );
 
 -- Constraints
@@ -89,17 +92,18 @@ ALTER TABLE stage
   ADD CONSTRAINT stage_fk_2 FOREIGN KEY (control_entry_id) REFERENCES control_entry(id),
   ADD CONSTRAINT stage_fk_3 FOREIGN KEY (next_stage_id) REFERENCES stage(id),
   ADD CONSTRAINT stage_ck_1 CHECK (
-  (question_type = 'DECONTROL' AND next_stage_id IS NOT NULL) OR question_type != 'DECONTROL'
-);
+    (question_type = 'DECONTROL' AND (next_stage_id IS NOT NULL OR go_to_stage_outcome_type = 'CONTROL_ENTRY_FOUND'))
+    OR (question_type != 'DECONTROL' and go_to_stage_outcome_type IS NULL)
+  );
 
 ALTER TABLE stage_answer
   ADD CONSTRAINT stage_answer_fk_1 FOREIGN KEY (parent_stage_id) REFERENCES stage(id),
   ADD CONSTRAINT stage_answer_fk_2 FOREIGN KEY (go_to_stage_id) REFERENCES stage(id),
   ADD CONSTRAINT stage_answer_fk_3 FOREIGN KEY (control_entry_id) REFERENCES control_entry(id),
   ADD CONSTRAINT stage_answer_ck_1 CHECK (
-    (go_to_stage_id IS NULL AND go_to_outcome_type IS NOT NULL)
+    (go_to_stage_id IS NULL AND go_to_stage_answer_outcome_type IS NOT NULL)
     OR
-    (go_to_outcome_type IS NULL AND go_to_stage_id IS NOT NULL)
+    (go_to_stage_answer_outcome_type IS NULL AND go_to_stage_id IS NOT NULL)
   ),
   ADD CONSTRAINT stage_answer_ck_2 CHECK (
     (control_entry_id IS NULL AND answer_text IS NOT NULL) OR control_entry_id IS NOT NULL
