1d69bfbf56cd46dee5e0ebf8046f66a36eda4496
Stefan Brach <brach.stefan@gmail.com>
2018-05-25 16:55:27 +0100

    PF-203 Implemented view more information

--- app/components/services/AnswerViewViewServiceImpl.java
+++ app/components/services/AnswerViewViewServiceImpl.java
@@ -40,25 +40,54 @@ public class AnswerViewViewServiceImpl implements AnswerViewService {
   private AnswerView createAnswerView(AnswerConfig answerConfig) {
     Optional<ControlEntryConfig> associatedControlEntryConfig = answerConfig.getAssociatedControlEntryConfig();
     if (associatedControlEntryConfig.isPresent()) {
-      // consider nested children
-      ControlEntryConfig controlEntryConfig = associatedControlEntryConfig.get();
-      List<SubAnswerView> subAnswerViews = createSubAnswerViews(controlEntryConfig);
-      if (answerConfig.getNestedContent().isPresent()) {
-        subAnswerViews = new ArrayList<>();
-      }
-      String nestedContent = createdNestedContent(answerConfig);
-      return new AnswerView(htmlRenderService.convertRichTextToPlainText(controlEntryConfig.getFullDescription()),
-          answerConfig.getAnswerId(), answerConfig.isDividerAbove(), subAnswerViews, nestedContent);
+      return createAnswerViewFromControlEntryConfig(answerConfig, associatedControlEntryConfig.get());
     } else {
       Optional<RichText> labelText = answerConfig.getLabelText();
       if (labelText.isPresent()) {
-        String nestedContent = createdNestedContent(answerConfig);
-        return new AnswerView(htmlRenderService.convertRichTextToPlainText(labelText.get()), answerConfig.getAnswerId(),
-            answerConfig.isDividerAbove(), new ArrayList<>(), nestedContent);
+        return createAnswerViewFromLabelText(answerConfig, labelText.get());
       } else {
         throw new BusinessRuleException("Both answerConfig.getAssociatedControlEntryConfig and answerConfig.getLabelText are absent.");
       }
     }
+  }
+
+  private AnswerView createAnswerViewFromControlEntryConfig(AnswerConfig answerConfig,
+                                                            ControlEntryConfig controlEntryConfig) {
+    String prompt = htmlRenderService.convertRichTextToPlainText(controlEntryConfig.getFullDescription());
+    List<SubAnswerView> subAnswerViews;
+    if (answerConfig.getNestedContent().isPresent()) {
+      subAnswerViews = new ArrayList<>();
+    } else {
+      subAnswerViews = createSubAnswerViews(controlEntryConfig);
+    }
+    String moreInformation;
+    if (!subAnswerViews.isEmpty()) {
+      moreInformation = "";
+    } else {
+      Optional<String> nextStageId = answerConfig.getNextStageId();
+      if (nextStageId.isPresent()) {
+        moreInformation = createMoreInformation(nextStageId.get());
+      } else {
+        moreInformation = "";
+      }
+    }
+    String nestedContent = createdNestedContent(answerConfig);
+    return new AnswerView(prompt, answerConfig.getAnswerId(), answerConfig.isDividerAbove(), subAnswerViews,
+        nestedContent, moreInformation);
+  }
+
+  private AnswerView createAnswerViewFromLabelText(AnswerConfig answerConfig, RichText labelText) {
+    String prompt = htmlRenderService.convertRichTextToPlainText(labelText);
+    String nestedContent = createdNestedContent(answerConfig);
+    String moreInformation;
+    Optional<String> nextStageId = answerConfig.getNextStageId();
+    if (nextStageId.isPresent()) {
+      moreInformation = createMoreInformation(nextStageId.get());
+    } else {
+      moreInformation = "";
+    }
+    return new AnswerView(prompt, answerConfig.getAnswerId(), answerConfig.isDividerAbove(), new ArrayList<>(),
+        nestedContent, moreInformation);
   }
 
   @Override
@@ -70,6 +99,53 @@ public class AnswerViewViewServiceImpl implements AnswerViewService {
     } else {
       return new ArrayList<>();
     }
+  }
+
+  private String createMoreInformation(String stageId) {
+    StageConfig stageConfig = journeyConfigService.getStageConfigById(stageId);
+    if (stageConfig.getQuestionType() == StageConfig.QuestionType.STANDARD) {
+      StringBuilder stringBuilder = new StringBuilder();
+      stringBuilder.append("<ul>");
+      for (AnswerConfig answerConfig : stageConfig.getAnswerConfigs()) {
+        stringBuilder.append("<li>");
+        Optional<ControlEntryConfig> associatedControlEntryConfig = answerConfig.getAssociatedControlEntryConfig();
+        if (associatedControlEntryConfig.isPresent()) {
+          ControlEntryConfig controlEntryConfig = associatedControlEntryConfig.get();
+          stringBuilder.append(htmlRenderService.convertRichTextToPlainText(controlEntryConfig.getFullDescription()));
+          String subAnswerViewsToHtml = subAnswerViewsToHtml(createSubAnswerViews(controlEntryConfig));
+          stringBuilder.append(subAnswerViewsToHtml);
+        } else if (answerConfig.getLabelText().isPresent()) {
+          stringBuilder.append(htmlRenderService.convertRichTextToHtml(answerConfig.getLabelText().get()));
+        } else {
+          throw new BusinessRuleException("Both answerConfig.getAssociatedControlEntryConfig and answerConfig.getLabelText are absent.");
+        }
+        stringBuilder.append("</li>");
+      }
+      stringBuilder.append("</ul>");
+      return stringBuilder.toString();
+    } else {
+      Optional<String> nextStageId = stageConfig.getNextStageId();
+      if (nextStageId.isPresent()) {
+        return createMoreInformation(nextStageId.get());
+      } else {
+        return "";
+      }
+    }
+  }
+
+  private String subAnswerViewsToHtml(List<SubAnswerView> subAnswerViews) {
+    StringBuilder stringBuilder = new StringBuilder();
+    if (!subAnswerViews.isEmpty()) {
+      stringBuilder.append("<ul>");
+      for (SubAnswerView subAnswerView : subAnswerViews) {
+        stringBuilder.append("<li>");
+        stringBuilder.append(subAnswerView.getText());
+        stringBuilder.append("</li>");
+        stringBuilder.append(subAnswerViewsToHtml(subAnswerView.getSubAnswerViews()));
+      }
+      stringBuilder.append("</ul>");
+    }
+    return stringBuilder.toString();
   }
 
   private String createdNestedContent(AnswerConfig answerConfig) {
