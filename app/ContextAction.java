1562aa51f9c2be9241a133b68ac5355e79aabb50
Pete Nattress <pete.nattress@fivium.co.uk>
2018-06-19 17:16:45 +0100

    Remove ContextParamManager from ContextAction

--- app/ContextAction.java
+++ app/ContextAction.java
@@ -1,6 +1,5 @@
 import com.google.inject.Inject;
 import components.common.CommonContextActionSetup;
-import components.common.state.ContextParamManager;
 import play.i18n.MessagesApi;
 import play.mvc.Action;
 import play.mvc.Http;
@@ -10,20 +9,15 @@ import java.util.concurrent.CompletionStage;
 
 public class ContextAction extends Action<Void> {
 
-  private final ContextParamManager contextParamManager;
   private final MessagesApi messagesApi;
 
   @Inject
-  public ContextAction(ContextParamManager contextParamManager, MessagesApi messagesApi) {
-    this.contextParamManager = contextParamManager;
+  public ContextAction(MessagesApi messagesApi) {
     this.messagesApi = messagesApi;
   }
 
   @Override
   public CompletionStage<Result> call(Http.Context ctx) {
-
-    // Add a reference to the ContextParamManager to the context so views can see it (DI workaround)
-    ctx.args.put(ContextParamManager.CTX_PARAM_NAME, contextParamManager);
 
     ctx.args.put(CommonContextActionSetup.CTX_MESSAGE_API_NAME, messagesApi);
 
