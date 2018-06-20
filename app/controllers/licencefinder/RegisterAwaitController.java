90e85b8ad7cdad2baac77cc3f08b07e2a2daead0
Dan Haynes <djm.haynes@gmail.com>
2018-06-20 16:37:29 +0100

    Tidy

--- app/controllers/licencefinder/RegisterAwaitController.java
+++ app/controllers/licencefinder/RegisterAwaitController.java
@@ -5,13 +5,11 @@ import static java.util.concurrent.CompletableFuture.completedFuture;
 import com.google.inject.Inject;
 import components.auth.SamlAuthorizer;
 import components.common.auth.SpireSAML2Client;
-import components.common.cache.CountryProvider;
 import components.persistence.LicenceFinderDao;
 import components.services.LicenceFinderService;
 import components.services.OgelService;
 import models.view.RegisterResultView;
 import org.pac4j.play.java.Secure;
-import play.data.FormFactory;
 import play.libs.concurrent.HttpExecutionContext;
 import play.mvc.Controller;
 import play.mvc.Result;
@@ -19,55 +17,32 @@ import play.mvc.Result;
 import java.util.Optional;
 import java.util.concurrent.CompletionStage;
 
-import javax.inject.Named;
-
 @Secure(clients = SpireSAML2Client.CLIENT_NAME, authorizers = SamlAuthorizer.AUTHORIZER_NAME)
-//@With(UserGuardAction.class)
 public class RegisterAwaitController extends Controller {
 
-  private static final String CONTROL_CODE_QUESTION = "What Control list entry describes your goods?";
-  private static final String GOODS_GOING_QUESTION = "Where are your goods going?";
-  private static final String FIRST_COUNTRY = "First country or territory that will receive the items";
-  private static final String REPAIR_QUESTION = "Are you exporting goods for or after repair or replacement?";
-  private static final String EXHIBITION_QUESTION = "Are you exporting goods for or after exhibition or demonstration?";
-  private static final String BEFORE_OR_LESS_QUESTION = "Were your goods manufactured before 1897, and worth less than £30,000?";
-
-  private static final String YES = "Yes";
-  private static final String NO = "No";
-
-  private final FormFactory formFactory;
   private final LicenceFinderDao licenceFinderDao;
-  private final CountryProvider countryProvider;
   private final HttpExecutionContext httpContext;
   private final String dashboardUrl;
   private final OgelService ogelService;
   private final LicenceFinderService licenceFinderService;
   private final views.html.licencefinder.registerResult registerResult;
-  private final views.html.licencefinder.registerToUse registerToUse;
   private final views.html.licencefinder.registerWait registerWait;
 
   @Inject
-  public RegisterAwaitController(FormFactory formFactory,
-                                 HttpExecutionContext httpContext,
+  public RegisterAwaitController(HttpExecutionContext httpContext,
                                  LicenceFinderDao licenceFinderDao,
-                                 @Named("countryProviderExport") CountryProvider countryProvider,
                                  @com.google.inject.name.Named("dashboardUrl") String dashboardUrl,
                                  OgelService ogelService, LicenceFinderService licenceFinderService,
                                  views.html.licencefinder.registerResult registerResult,
-                                 views.html.licencefinder.registerToUse registerToUse,
                                  views.html.licencefinder.registerWait registerWait) {
-    this.formFactory = formFactory;
     this.httpContext = httpContext;
     this.licenceFinderDao = licenceFinderDao;
-    this.countryProvider = countryProvider;
     this.dashboardUrl = dashboardUrl;
     this.ogelService = ogelService;
     this.licenceFinderService = licenceFinderService;
     this.registerResult = registerResult;
-    this.registerToUse = registerToUse;
     this.registerWait = registerWait;
   }
-
 
   /**
    * renderAwaitResult
