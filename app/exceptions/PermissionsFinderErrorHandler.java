83647cf69782858fd4a49bd9078e3fe0da463cda
Pete Nattress <pete.nattress@fivium.co.uk>
2018-07-26 10:55:44 +0100

    PF-365 Server error content update

--- app/exceptions/PermissionsFinderErrorHandler.java
+++ app/exceptions/PermissionsFinderErrorHandler.java
@@ -1,9 +1,9 @@
 package exceptions;
 
-import static play.mvc.Results.badRequest;
 import static play.mvc.Results.notFound;
 
 import com.google.inject.Inject;
+import com.google.inject.name.Named;
 import com.typesafe.config.Config;
 import controllers.common.ErrorHandler;
 import org.apache.commons.lang3.exception.ExceptionUtils;
@@ -24,8 +24,9 @@ public class PermissionsFinderErrorHandler extends ErrorHandler {
 
   @Inject
   public PermissionsFinderErrorHandler(Environment environment, OptionalSourceMapper sourceMapper,
-                                       Config config, views.html.licencefinder.notFound notFound) {
-    super(environment, sourceMapper, config);
+                                       Config config, views.html.licencefinder.notFound notFound,
+                                       @Named("ecjuEmailAddress") String errorContactEmail) {
+    super(environment, sourceMapper, config, errorContactEmail);
     this.notFound = notFound;
   }
 
