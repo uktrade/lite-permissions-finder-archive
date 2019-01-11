e83db8a7dff162513cb7171dd3c58bb091c82a97
Jakub Kozlowski <jakub.kozlowski@digital.trade.gov.uk>
2019-01-11 14:02:32 +0000

    LT-675: more whitespace changes

--- app/controllers/StartApplicationController.java
+++ app/controllers/StartApplicationController.java
@@ -17,69 +17,73 @@ import static play.mvc.Results.redirect;
 
 public class StartApplicationController {
 
-    private static final org.slf4j.Logger LOGGER = LoggerFactory.getLogger(StartApplicationController.class);
+  private static final org.slf4j.Logger LOGGER = LoggerFactory.getLogger(StartApplicationController.class);
 
-    private final FormFactory formFactory;
-    private final SessionService sessionService;
-    private final PermissionsFinderNotificationClient permissionsFinderNotificationClient;
-    private final views.html.startApplication startApplication;
+  private final FormFactory formFactory;
+  private final SessionService sessionService;
+  private final PermissionsFinderNotificationClient permissionsFinderNotificationClient;
+  private final views.html.startApplication startApplication;
 
-    @Inject
-    public StartApplicationController(FormFactory formFactory, SessionService sessionService,
-                                      PermissionsFinderNotificationClient permissionsFinderNotificationClient,
-                                      views.html.startApplication startApplication) {
-        this.formFactory = formFactory;
-        this.sessionService = sessionService;
-        this.permissionsFinderNotificationClient = permissionsFinderNotificationClient;
-        this.startApplication = startApplication;
+  @Inject
+  public StartApplicationController(FormFactory formFactory, SessionService sessionService,
+      PermissionsFinderNotificationClient permissionsFinderNotificationClient,
+      views.html.startApplication startApplication) {
+    this.formFactory = formFactory;
+    this.sessionService = sessionService;
+    this.permissionsFinderNotificationClient = permissionsFinderNotificationClient;
+    this.startApplication = startApplication;
+  }
+
+  public Result createApplication() {
+    if (StringUtils.isNoneBlank(flash("error"))) {
+      flash("error", flash("error"));
+      flash("detail", flash("detail"));
     }
 
-    public Result createApplication() {
-        if (StringUtils.isNoneBlank(flash("error"))) {
-            flash("error", flash("error"));
-            flash("detail", flash("detail"));
-        }
+    TriageSession triageSession = sessionService.createNewSession();
+    return redirect(
+        routes.StartApplicationController.renderStartApplication(triageSession.getId()));
+  }
 
-        TriageSession triageSession = sessionService.createNewSession();
-        return redirect(routes.StartApplicationController.renderStartApplication(triageSession.getId()));
+  public Result renderStartApplication(String sessionId) {
+    TriageSession triageSession = sessionService.getSessionById(sessionId);
+
+    // If the session isn't valid, log and redirect to new application
+    if (triageSession == null) {
+      LOGGER.error("Unknown sessionId {}", sessionId);
+      return redirect(routes.StartApplicationController.createApplication());
     }
 
-    public Result renderStartApplication(String sessionId) {
-        TriageSession triageSession = sessionService.getSessionById(sessionId);
+    return ok(
+        startApplication.render(formFactory.form(StartApplicationForm.class), triageSession.getId(),
+            triageSession.getResumeCode()));
+  }
 
-        // If the session isn't valid, log and redirect to new application
-        if (triageSession == null) {
-            LOGGER.error("Unknown sessionId {}", sessionId);
-            return redirect(routes.StartApplicationController.createApplication());
-        }
+  public Result handleSubmit(String sessionId) {
+    TriageSession triageSession = sessionService.getSessionById(sessionId);
+    Form<StartApplicationForm> form = formFactory.form(StartApplicationForm.class)
+        .bindFromRequest();
+    String emailAddress = form.get().emailAddress;
 
-        return ok(startApplication.render(formFactory.form(StartApplicationForm.class), triageSession.getId(),
-                triageSession.getResumeCode()));
+    // If the session isn't valid, log and redirect to new application
+    if (triageSession == null) {
+      LOGGER.error("Unknown sessionId {}", sessionId);
+      return redirect(routes.StartApplicationController.createApplication());
     }
 
-    public Result handleSubmit(String sessionId) {
-        TriageSession triageSession = sessionService.getSessionById(sessionId);
-        Form<StartApplicationForm> form = formFactory.form(StartApplicationForm.class).bindFromRequest();
-        String emailAddress = form.get().emailAddress;
-
-        // If the session isn't valid, log and redirect to new application
-        if (triageSession == null) {
-            LOGGER.error("Unknown sessionId {}", sessionId);
-            return redirect(routes.StartApplicationController.createApplication());
-        }
-
-        // If the form has errors, return to form and show error
-        if (form.hasErrors()) {
-            return ok(startApplication.render(form, triageSession.getId(), triageSession.getResumeCode()));
-        }
-
-        // If the user has entered an email address, send them the application reference
-        if (StringUtils.isNoneBlank(emailAddress)) {
-            String resumeCode = triageSession.getResumeCode();
-            permissionsFinderNotificationClient.sendApplicationReferenceEmail(emailAddress.trim(), resumeCode);
-        }
-
-        return redirect(routes.OnboardingController.renderForm(sessionId));
+    // If the form has errors, return to form and show error
+    if (form.hasErrors()) {
+      return ok(
+          startApplication.render(form, triageSession.getId(), triageSession.getResumeCode()));
     }
 
+    // If the user has entered an email address, send them the application reference
+    if (StringUtils.isNoneBlank(emailAddress)) {
+      String resumeCode = triageSession.getResumeCode();
+      permissionsFinderNotificationClient
+          .sendApplicationReferenceEmail(emailAddress.trim(), resumeCode);
+    }
+
+    return redirect(routes.OnboardingController.renderForm(sessionId));
+  }
 }
